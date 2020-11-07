ed3444e4276e67b37e7212f0fd72bb8d4aa1c9ac
Gustavo Tatarem <gus.tatarem@gmail.com>
2020-11-07 12:24:02 -0300

    finish orders page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -61,7 +61,7 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      const response = await api.get('foods', {
+      const response = await api.get('/foods', {
         params: {
           category_like: selectedCategory,
           name_like: searchValue,
